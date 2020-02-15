48c08ab4980f052b0d46122a8ef3df6d344340bf
BiggaHD <dzenis.hankusic@gmail.com>
2020-02-15 07:08:22 +0100

    Improve UI + prsist token to LS

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
-import { Router } from '@angular/router';
-import * as firebase from 'firebase';
-import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import * as firebase from "firebase";
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
@@ -9,45 +9,46 @@ export class AuthService {
   constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(
-      () => {
-        this.router.navigate(['/signin']);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.router.navigate(["/signin"]);
       })
-      .catch(
-        (error: any) => window.alert(error)
-      )
+      .catch((error: any) => window.alert(error));
   }
 
   signinUser(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-        () => {
-          this.router.navigate(['/']);
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => this.token = token
-            )
-        }
-      ).catch(
-        (error: any) => window.alert(error)
-      );
-    }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.router.navigate(["/"]);
+        firebase
+          .auth()
+          .currentUser.getIdToken()
+          .then((token: string) => (this.token = token))
+          .then(() => localStorage.setItem("token", this.token));
+      })
+      .catch((error: any) => window.alert(error));
+  }
 
   logout() {
     firebase.auth().signOut();
     this.token = null;
+    localStorage.setItem("token", "");
   }
 
   getIdToken() {
-    firebase.auth().currentUser.getIdToken()
-      .then(
-        (token: string) => this.token = token
-      );
+    firebase
+      .auth()
+      .currentUser.getIdToken()
+      .then((token: string) => (this.token = token));
     return this.token;
   }
 
   isAuthenticated() {
-    return this.token != null;
+    const xo = localStorage.getItem("token");
+    return this.token != null || xo != "";
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,43 +1,44 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/Rx";
 
-import { RecipeService } from '../recipes/recipe.service';
-import { Recipe } from '../recipes/recipe.model';
-import { AuthService } from '../auth/auth.service';
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
-              private recipeService: RecipeService,
-              private authService: AuthService) {
-  }
+  constructor(
+    private http: Http,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const token = this.authService.getIdToken();
 
-    return this.http.put('https://ng-recipes-f4e6c.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.http.put(
+      "https://balkan-recipes.firebaseapp.com/recipes.json?auth=" + token,
+      this.recipeService.getRecipes()
+    );
   }
 
   getRecipes() {
     const token = this.authService.getIdToken();
 
-    this.http.get('https://ng-recipes-f4e6c.firebaseio.com/recipes.json?auth=' + token)
-      .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
-          for (let recipe of recipes) {
-            if (!recipe['ingredients']) {
-              recipe['ingredients'] = [];
-            }
+    this.http
+      .get("https://balkan-recipes.firebaseapp.com/recipes.json?auth=" + token)
+      .map((response: Response) => {
+        const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe["ingredients"]) {
+            recipe["ingredients"] = [];
           }
-          return recipes;
         }
-      )
-      .subscribe(
-        (recipes: Recipe[]) => {
-          this.recipeService.setRecipes(recipes);
-        }
-      );
+        return recipes;
+      })
+      .subscribe((recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
